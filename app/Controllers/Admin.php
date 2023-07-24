c5e8475d6dc0815cde6f385bc503a52532ef2f7c
Andriyan <andriyann77@gmail.com>
2023-07-24 19:33:59 +0800

    done dashboard

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,12 +2,46 @@
 
 namespace App\Controllers;
 
+use App\Models\Mod_user;
+use App\Models\Mod_ikan;
+use App\Models\Mod_penjual;
+use App\Models\Mod_pasar;
+use App\Models\Mod_pembenihan;
+use App\Models\Mod_hargaikan;
+use App\Models\Mod_pembesaran;
+use App\Models\Mod_tangkap_ikan;
+use App\Models\Mod_transaksi_ikan;
+use App\Models\Mod_berita;
+use App\Models\Mod_pengolahan;
+
 class Admin extends BaseController
 {
     protected $session;
+    protected $Mod_user;
+    protected $Mod_penjual;
+    protected $Mod_ikan;
+    protected $Mod_pengolahan;
+    protected $Mod_berita;
+    protected $Mod_transaksi_ikan;
+    protected $Mod_hargaikan;
+    protected $Mod_pembesaran;
+    protected $Mod_tangkap_ikan;
+    protected $Mod_pembenihan;
+    protected $Mod_pasar;
     public function __construct()
     {
         $this->session = session();
+        $this->Mod_user = new Mod_user();
+        $this->Mod_transaksi_ikan = new Mod_transaksi_ikan();
+        $this->Mod_ikan = new Mod_ikan();
+        $this->Mod_hargaikan = new Mod_hargaikan();
+        $this->Mod_berita = new Mod_berita();
+        $this->Mod_pembesaran = new Mod_pembesaran();
+        $this->Mod_tangkap_ikan = new Mod_tangkap_ikan();
+        $this->Mod_pengolahan = new Mod_pengolahan();
+        $this->Mod_pembenihan = new Mod_pembenihan();
+        $this->Mod_penjual = new Mod_penjual();
+        $this->Mod_pasar = new Mod_pasar();
     }
 
     public function index()
@@ -21,10 +55,33 @@ class Admin extends BaseController
         if ($this->session->get('hak_akses') != 1) {
             return redirect()->to('/user');
         }
+        $user = $this->Mod_user->countAllResults();
+        $ikan = $this->Mod_ikan->countAllResults();
+        $pasar = $this->Mod_pasar->countAllResults();
+        $penjual = $this->Mod_penjual->countAllResults();
+        $berita = $this->Mod_berita->countAllResults();
+        $trans = $this->Mod_transaksi_ikan->countAllResults();
+        $olah = $this->Mod_pengolahan->countAllResults();
+        $hargaikan = $this->Mod_hargaikan->countAllResults();
+        $besar = $this->Mod_pembesaran->countAllResults();
+        $benih = $this->Mod_pembenihan->countAllResults();
+        $tangkap = $this->Mod_tangkap_ikan->countAllResults();
         $data = [
             'title' => 'Nama',
             'type' => 'Dashboard',
-            'head' => 'Sistem informasi ketersediaan harga ikan di pasar pada kantor dinas perikanan(DISKAN) Rantau Kabupaten Tapin Berbasis WEB',
+            'head' => 'SISTEM INFORMASI KETERSEDIAAN IKAN DI PASAR PADA KANTOR DINAS PERIKANAN (DISKAN) RANTAU KEBUPATEN TAPIN BERBASIS WEB',
+            'user' => $user,
+            'ikan' => $ikan,
+            'pasar' => $pasar,
+            'penjual' => $penjual,
+            'hargaikan' => $hargaikan,
+            'benih' => $benih,
+            'besar' => $besar,
+            'tangkap' => $tangkap,
+            'olah' => $olah,
+            'trans' => $trans,
+            'berita' => $berita,
+
         ];
 
         return view('admin/index', $data);
--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -1,64 +1,194 @@
-            <?= $this->include('nav/head'); ?>
-            <div style="display: none;">
+<?= $this->include('nav/head'); ?>
+<style>
+    .img {
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        padding: 5px;
+        width: 100%;
+        height: 200px;
+        filter: drop-shadow(8px 8px 10px #000);
+        object-fit: cover;
+        object-position: 20% 70%;
+        display: block;
+        margin-bottom: 20px;
+    }
 
-                <!-- // -------------------------------------------------------------------------------------------------------------- -->
-                <form action="" method="post">
-                    <textarea name="textarea" id="default"></textarea>
-                </form>
-                <!-- // -------------------------------------------------------------------------------------------------------------- -->
-                <div class="card mb-4">
+    @keyframes example {
+        0% {
+            width: 100%;
+            height: 200px;
+        }
+
+        100% {
+            background-color: black;
+            width: 100%;
+            height: 70vh;
+        }
+    }
+
+    .img:hover {
+        animation-name: example;
+        animation-duration: 4s;
+        /* animation-iteration-count: infinite; */
+        animation-fill-mode: forwards;
+        position: relative;
+    }
+</style>
+<?= $this->include('alert/index'); ?>
+<img src="<?= base_url('src/img/'); ?>image.jpg" class="img">
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
                     <div class="card-header">
-                        <i class="fas fa-table me-1"></i>
-                        DataTable Example
+                        <b>
+                            Data Pnegguna
+                        </b>
                     </div>
                     <div class="card-body">
-                        <table id="tb1">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Office</th>
-                                    <th>Age</th>
-                                    <th>Start date</th>
-                                    <th>Salary</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Tiger Nixon</td>
-                                    <td>System Architect</td>
-                                    <td>Edinburgh</td>
-                                    <td>61</td>
-                                    <td>2011/04/25</td>
-                                    <td>$320,800</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <h1><?= $user; ?></h1> Data Di Dapat
                     </div>
                 </div>
             </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $ikan; ?></h1> Data Di Dapat
+                    </div>
+                </div>
             </div>
-            <?= $this->include('nav/foot'); ?>
-            <script>
-                tinymce.init({
-                    selector: '#default',
-                    width: 1000,
-                    height: 300,
-                    plugins: [
-                        'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'prewiew', 'anchor', 'pagebreak',
-                        'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media',
-                        'table', 'emoticons', 'template', 'codesample'
-                    ],
-                    toolbar: 'undo redo | styles | bold italic underline | alignleft aligncenter alignright alignjustify |' +
-                        'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
-                        'forecolor backcolor emoticons',
-                    menu: {
-                        favs: {
-                            title: 'menu',
-                            items: 'code visualaid | searchreplace | emoticons'
-                        }
-                    },
-                    menubar: 'favs file edit view insert format tools table',
-                    content_style: 'body{font-family:Helvetica,Arial,sans-serif; font-size:16px}'
-                });
-            </script>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Pasar
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $pasar; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Penjual
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $penjual; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Harga Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $hargaikan; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Pembenihan Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $benih; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Pembesaran Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $besar; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Penangkapan Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $tangkap; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Pengolahan Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $olah; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Keluar/Masuk Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $trans; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Berita
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $berita; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">
+                        <b>
+                            Data Penangkapan Ikan
+                        </b>
+                    </div>
+                    <div class="card-body">
+                        <h1><?= $tangkap; ?></h1> Data Di Dapat
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<?= $this->include('nav/foot'); ?>
